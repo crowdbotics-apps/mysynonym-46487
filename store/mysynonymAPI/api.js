308a9c9f6820f1c4e535edec4f018920860648af
Crowdbotics <engineering@crowdbotics.com>
2024-02-21 07:41:54 +0000

    Update store.

--- store/mysynonymAPI/api.js
+++ store/mysynonymAPI/api.js
@@ -14,6 +14,24 @@ function api_v1_login_create(payload) {
 function api_v1_signup_create(payload) {
   return mysynonymAPI.post(`/api/v1/signup/`, payload)
 }
+function api_v1_test_list(payload) {
+  return mysynonymAPI.get(`/api/v1/test/`)
+}
+function api_v1_test_create(payload) {
+  return mysynonymAPI.post(`/api/v1/test/`, payload)
+}
+function api_v1_test_retrieve(payload) {
+  return mysynonymAPI.get(`/api/v1/test/${payload.id}/`)
+}
+function api_v1_test_update(payload) {
+  return mysynonymAPI.put(`/api/v1/test/${payload.id}/`, payload)
+}
+function api_v1_test_partial_update(payload) {
+  return mysynonymAPI.patch(`/api/v1/test/${payload.id}/`, payload)
+}
+function api_v1_test_destroy(payload) {
+  return mysynonymAPI.delete(`/api/v1/test/${payload.id}/`)
+}
 function rest_auth_login_create(payload) {
   return mysynonymAPI.post(`/rest-auth/login/`, payload)
 }
@@ -51,6 +69,12 @@ export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
   api_v1_signup_create,
+  api_v1_test_list,
+  api_v1_test_create,
+  api_v1_test_retrieve,
+  api_v1_test_update,
+  api_v1_test_partial_update,
+  api_v1_test_destroy,
   rest_auth_login_create,
   rest_auth_logout_create,
   rest_auth_password_change_create,
